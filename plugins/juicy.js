11c5dc66e227b8efae6433251b5051fddb442242
Paul Dariye <paul.dariye@gmail.com>
2018-01-01 20:25:49 -0500

    Add juicy plugin

---
+++ plugins/juicy.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+export default ({app}) => {
+  if (!adsbyjuicy && !window.adsbyjuicy) {
+    var adsbyjuicy = window.adsbyjuicy || []
+    adsbyjuicy.push({ 'adzone': 624872 })
+  }
+}
